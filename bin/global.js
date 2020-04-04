744b3c7e1146c4494abb79584cb196115d859d48
Andrei Cracanau <andithemudkip@gmail.com>
2020-04-05 00:20:00 +0300

    idk

--- bin/global.js
+++ bin/global.js
@@ -1,37 +1,42 @@
 #!/usr/bin/env node
 
-const commandLineArgs = require ('command-line-args')
-const commandLineUsage = require ('command-line-usage')
-
-const gml = require ('../lib/index');
+const commandLineArgs = require ('command-line-args');
+const commandLineUsage = require ('command-line-usage');
+const path = require ('path');
+const { compile } = require ('../lib/index');
 
 const optionDefinitions = [{
-      name: 'help',
-      alias: 'h',
-      type: Boolean,
-      description: 'Display this usage guide.'
+        name: 'help',
+        alias: 'h',
+        type: Boolean,
+        description: 'Display this usage guide.'
     }, {
-      name: 'src',
-      alias: 's',
-      type: String,
-      multiple: false,
-      description: 'The input file to compile',
-      typeLabel: '<file>'
+        name: 'src',
+        alias: 's',
+        type: String,
+        multiple: false,
+        description: 'The input file to compile',
+        typeLabel: '<file>'
     }, {
         name: 'output',
         alias: 'o',
         type: String,
         multiple: false,
-        description: 'The output filename (default: <input_filename>.gameml.js)'
+        description: 'The output filename (default: <input_filename>.js)',
+        typeLabel: '<file>'
+    }, {
+        name: 'serve',
+        type: Number,
+        typeLabel: '<port>',
+        description: 'Start a server on the specified port hosting the compiled game'
     }]
 
 const options = commandLineArgs (optionDefinitions);
 
-
 if (options.help) {
     const usage = commandLineUsage ([{
             header: "Game Markup Language",
-            content: "A CLI for compiling gameml files to JavaScript",
+            content: "A CLI for compiling game-ml files to JavaScript",
         }, {
             header: "Options",
             optionList: optionDefinitions,
@@ -39,11 +44,19 @@ if (options.help) {
             content: "Project home: {underline https://github.com/andithemudkip/game-ml}",
     }]);
     console.log (usage);
+} else if (options.src) {
+    let inputPath = path.isAbsolute (options.src) ? options.src : path.join (process.cwd (), options.src);
+    let outputPath = options.output && (path.isAbsolute (options.output) ? options.output : path.join (process.cwd (), options.output)) || inputPath + '.js';
+
+    console.log (inputPath, '->', outputPath);
+
+    compile (inputPath, outputPath);
+
+
+    if (options.serve) {
+        // start server after compiling
+    }
+
 } else {
     console.log (options);
-    console.log (process.argv);
 }
-
-
-// lib.doStuff ();
-// console.)
---
+++ example/main.gml
@@ -0,0 +1,10 @@
+<setup>
+    <var score>0</var>
+    <world id = 1>
+        <text>${score}</text>
+        <actor
+            name = "andi"
+            age = 19
+        />
+    </world>
+</setup>
